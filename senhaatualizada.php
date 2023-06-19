0fc651bd352f0b15cd0e5718ab4591567ca073ac
rebecaalvesp <128102355+rebecaalvesp@users.noreply.github.com>
2023-06-19 20:56:24 -0300

    Create senhaatualizada.php

---
+++ senhaatualizada.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Senha Atualizada</title>
+    <style>
+        body {
+            background-image: url(../WW/chuva.jpg);
+            background-size: cover;
+            background-position: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        center {
+         background-color: rgba(0, 0, 0,0.5);
+         position: absolute;
+         top: 50%;
+         left: 50%;
+         transform: translate(-50%,-50%);
+         padding: 80px;
+         border-radius: 15px;
+         color: black;
+        }
+
+        h1 {
+            margin-bottom: 20px;
+        }
+         .momo{
+            color:white;
+         }
+        a {
+            text-decoration: none;
+            color: #4CAF50;
+        }
+    </style>
+</head>
+<body>
+    <center>
+        <h1>Senha Atualizada com Sucesso!</h1>
+        <a href="login.php" class= "momo" >Voltar para a página de login</a>
+    </center>
+</body>
+</html>
